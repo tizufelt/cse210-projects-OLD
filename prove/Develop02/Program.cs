3a91cac44b823e7adb30041a1e4585a8f72c49c1
TimmyZ <106173110+tizufelt@users.noreply.github.com>
2023-01-20 20:27:40 -0500

    last of the C#

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,3 +7,24 @@ class Program
         Console.WriteLine("Hello Develop02 World!");
     }
 }
+
+class writeNRead //write and read into journal files
+
+
+class saveNLoad // load and save texxt file
+
+
+class prompt // prompt and display
+variables:
+private static bool MainMenu() { Console.Clear(); 
+Console.WriteLine("Choose an option:"); 
+Console.WriteLine("1) Reverse String"); 
+Console.WriteLine("2) Remove Whitespace"); 
+Console.WriteLine("3) Exit"); 
+Console.Write("\r\nSelect an option: "); 
+switch (Console.ReadLine()) { case "1": ReverseString(); 
+return true; 
+case "2": RemoveWhitespace(); 
+return true; 
+case "3": return false; 
+default: return true; } }
